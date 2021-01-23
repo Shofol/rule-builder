19e43c747c38993f2a1886f238894c0b488673eb
shofol <jahananower@gmail.com>
2021-01-24 02:34:03 +0600

    create rulebook

--- src/Rules/RuleContainer.js
+++ src/Rules/RuleContainer.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import RulebookForm from './RulebookForm';
 import RuleForm from './RuleForm'
 
 const Rules = () => {
     const [rules, setRules] = useState([]);
     const [showRuleForm, setShowRuleForm] = useState(false);
 
+    const [ruleBooks, setRuleBooks] = useState([]);
+    const ruleTabRef = useRef(null);
+    const ruleBookTabRef = useRef(null);
+
+    const [showRuleBookForm, setShowRuleBookForm] = useState(false);
+    const [selectedRule, setSelectedRule] = useState(null)
+
     useEffect(() => {
         updateRules();
-        // setTimeout(() => {
-        //     const rules = localStorage.getItem('rules');
-        //     if (rules) {
-        //         setRules(JSON.parse(rules));
-        //     }
-        // }, 100);
-
     }, []);
 
     const updateRules = () => {
@@ -25,6 +26,20 @@ const Rules = () => {
         }, 100);
     }
 
+    useEffect(() => {
+        updateRuleBooks();
+    }, []);
+
+    const updateRuleBooks = () => {
+        setTimeout(() => {
+            const ruleBooks = localStorage.getItem('ruleBooks');
+            if (ruleBooks) {
+                setRuleBooks(JSON.parse(ruleBooks));
+            }
+        }, 100);
+    }
+
+
 
     useEffect(() => {
         console.log(rules);
@@ -32,47 +47,100 @@ const Rules = () => {
         // localStorage.setItem('rules', JSON.stringify(rules));
     }, [rules]);
 
+    const onRuleDragstart = (ev, rule) => {
+        ev.dataTransfer.setData("rule", JSON.stringify(rule));
+    }
+
+    const drop = (ev) => {
+        ev.preventDefault();
+        // var data = ev.dataTransfer.getData("text");
+        setSelectedRule(ev.dataTransfer.getData("rule"))
+    }
+
+
     return (
-        <div>
-            <nav>
-                <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                    <a className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Rule</a>
-                    <a className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Rulebook</a>
-                </div>
-            </nav>
-            <div className="tab-content" id="nav-tabContent" style={{ maxWidth: '500px' }}>
-                <div className="tab-pane fade show active card" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" style={{ minHeight: '88.5vh' }}>
-                    <RuleForm updateRules={updateRules} />
-                    {rules.length === 0 && <h5 className="py-5 bg-light font-weight-bold">No rules found</h5>}
-                    {rules.length > 0 && <div className="accordion p-5" id="accordionFlushExample">
-                        {rules.map(
-                            (rule, index) => {
-                                return <div key={index} className="accordion-item">
-                                    <h2 className="accordion-header" id="flush-headingOne">
-                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapse${index}`} aria-expanded="false" aria-controls={`flush-collapse${index}`}>
-                                            {rule.name}
-                                        </button>
-                                    </h2>
-                                    <div id={`flush-collapse${index}`} className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                        <div className="accordion-body">
-                                            Description: {rule.description}
+        <div className="row">
+            <div className="col-3">
+                <nav>
+                    <div className="nav nav-tabs" id="nav-tab" role="tablist" style={{ borderRight: '1px solid #DFDFDF' }}>
+                        <a className="nav-link active" ref={ruleTabRef} id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Rule</a>
+                        <a className="nav-link" ref={ruleBookTabRef} id="nav-profile-tab" data-bs-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Rulebook</a>
+                    </div>
+                </nav>
+                <div className="tab-content" id="nav-tabContent" >
+                    <div className="tab-pane fade show active card" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" style={{ minHeight: '88.5vh' }}>
+                        <RuleForm updateRules={updateRules} />
+                        {rules.length === 0 && <h5 className="py-5 bg-light font-weight-bold">No rules found</h5>}
+                        {rules.length > 0 && <div className="accordion p-5" id="accordionFlushExample">
+                            {rules.map(
+                                (rule, index) => {
+                                    return <div key={index} className="accordion-item"
+                                        draggable="true" onDragStart={(ev) => onRuleDragstart(ev, rule)}
+                                        onDragOver={(event) => event.preventDefault()}>
+                                        <h2 className="accordion-header" id="flush-headingOne">
+                                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapse${index}`} aria-expanded="false" aria-controls={`flush-collapse${index}`}>
+                                                {rule.name}
+                                            </button>
+                                        </h2>
+                                        <div id={`flush-collapse${index}`} className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                            <div className="accordion-body">
+                                                Description: {rule.description}
                                             When: {rule.when}
                                             Effects:
                                             {
-                                                rule.effects.map(
-                                                    effect => {
-                                                        return <span>{effect}</span>
-                                                    }
-                                                )
-                                            }
+                                                    rule.effects.map(
+                                                        effect => {
+                                                            return <span>{effect}</span>
+                                                        }
+                                                    )
+                                                }
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            }
-                        )}
-                    </div>}
+                                }
+                            )}
+                        </div>}
+                    </div>
+                    <div className="tab-pane fade card p-5" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" style={{ minHeight: '88.5vh' }}>
+                        <input type="text" className="form-control" placeholder="Search a rulebook"></input>
+                        {ruleBooks.length === 0 && <h5 className="py-5 bg-light font-weight-bold mt-4">No rulebooks found</h5>}
+                        <button className="btn btn-primary mt-4" onClick={() => setShowRuleBookForm(true)}>Create a rulebook</button>
+
+                        {ruleBooks.length > 0 && <div className="accordion py-5" id="ruleBookAccordion">
+                            {ruleBooks.map(
+                                (ruleBook, index) => {
+                                    return <div key={index} className="accordion-item">
+                                        <h2 className="accordion-header" id="flush-headingOne">
+                                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapse${index}`} aria-expanded="false" aria-controls={`flush-collapse${index}`}>
+                                                {ruleBook.name}
+                                            </button>
+                                        </h2>
+                                        <div id={`flush-collapse${index}`} className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#ruleBookAccordion">
+                                            <div className="accordion-body">
+                                                Description: {ruleBook.description}
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                            )}
+                        </div>}
+                    </div>
                 </div>
-                <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+            </div>
+            <div className="col-9" onDrop={drop} onDragOver={(event) => event.preventDefault()}>
+                {showRuleBookForm &&
+                    <div >
+                        <RulebookForm updateRuleBooks={() => { updateRuleBooks() }}
+                            selectedRule={selectedRule}
+                            showRuleForm={showRuleForm} ruleBooksFromContainer={ruleBooks}
+                            showRules={() => { ruleTabRef.current.click() }}
+                            showRuleBooks={() => {
+                                ruleBookTabRef.current.click(); setTimeout(() => {
+                                    setShowRuleBookForm(false);
+                                    setSelectedRule(null)
+                                }, 100);
+                            }} /></div>
+                }
             </div>
         </div>
     )
---
+++ src/Rules/RulebookForm.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from 'react'
+import { Formik, Field } from 'formik';
+
+const RulebookForm = ({ updateRuleBooks, showRules, showRuleBooks, ruleBooksFromContainer, selectedRule }) => {
+
+    const [ruleBooks, setRuleBooks] = useState(ruleBooksFromContainer);
+    const [showRuleForm, setShowRuleForm] = useState(false);
+    const [currentRuleBook, setCurrentRuleBook] = useState(null)
+    const [currentRuleBookRules, setCurrentRuleBookRules] = useState([])
+
+    useEffect(() => {
+        localStorage.removeItem('ruleBooks')
+        localStorage.setItem('ruleBooks', JSON.stringify(ruleBooks));
+        updateRuleBooks();
+    }, [ruleBooks]);
+
+    useEffect(() => {
+        if (selectedRule) {
+            if (!(currentRuleBookRules.filter(rule => JSON.stringify(rule) === selectedRule).length > 0))
+                setCurrentRuleBookRules([...currentRuleBookRules, JSON.parse(selectedRule)]);
+        }
+    }, [selectedRule]);
+
+    const removeCurrentRule = (index) => {
+        console.log('removing');
+        const tempRules = [...currentRuleBookRules];
+        tempRules.splice(index, 1);
+        setCurrentRuleBookRules(tempRules);
+    }
+
+    const saveRuleBook = () => {
+
+        const newRuleBook = currentRuleBook;
+        newRuleBook.rules = currentRuleBookRules;
+
+        setRuleBooks([...ruleBooks, newRuleBook]);
+        showRuleBooks();
+    }
+
+    return (
+        <div className="card p-5 m-5 ">
+            <h3 className="text-start mb-4">New Rulebook</h3>
+            {!showRuleForm && <Formik
+                initialValues={{
+                    name: '',
+                    description: '',
+                    // when: '',
+                    // effects: ['']
+                }}
+                validate={values => {
+                    const errors = {};
+                    if (!values.name) {
+                        errors.name = 'Required';
+                    }
+
+                    if (!values.description) {
+                        errors.description = 'Required';
+                    }
+
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        setCurrentRuleBook(values);
+                        setShowRuleForm(true);
+                        showRules();
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting
+                }) => (
+                    <form className="d-flex flex-column align-items-start" onSubmit={handleSubmit} style={{ width: '100%' }}>
+                        <label htmlFor="name" className="form-label mt-2 text-left">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            className="form-control"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.name}
+                        />
+                        {errors.name && touched.name && errors.name}
+                        <label htmlFor="description" className="form-label mt-2">Description</label>
+                        <textarea
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.description}
+                        />
+                        {errors.description && touched.description && errors.description}
+                        {/* <label htmlFor="when" className="form-label mt-2">When</label>
+                        <input
+                            type="text"
+                            name="when"
+                            className="form-control"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.when}
+                        />
+                        {errors.when && touched.when && errors.when} */}
+
+                        <button type="submit" className="btn btn-success px-5 mt-4 ms-auto" disabled={isSubmitting}>
+                            Create Rulebook
+                        </button>
+                    </form>
+                )}</Formik>}
+            {showRuleForm && <div className="text-start"> <h5>{currentRuleBook.name}</h5>
+                {currentRuleBookRules.length === 0 &&
+                    <div className="py-5 bg-light font-weight-bold text-center"> <h5 >No rules added</h5>
+                        <p>Add a rule by dragging and dropping from rules tab</p>
+                    </div>}
+                {currentRuleBookRules.length > 0 && <div>
+                    <h6 className="mt-4">Rules</h6>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {currentRuleBookRules.map(
+                                (rule, index) => {
+                                    return <tr key={index}>
+                                        <th scope="row">{index + 1}</th>
+                                        <td>{rule.name}</td>
+                                        <td>{rule.description}</td>
+                                        <td><button className="btn btn-outline-secondary btn-sm"
+                                            onClick={() => { removeCurrentRule(index) }}>Remove</button></td>
+                                    </tr>
+
+                                }
+                            )}
+                        </tbody>
+                    </table>
+                    <button type="submit" className="btn btn-success px-5 mt-4 ms-auto" onClick={saveRuleBook}>
+                        Save Rulebook
+                        </button>
+                </div>
+                }
+            </div>
+            }
+        </div>
+    )
+}
+
+export default RulebookForm
